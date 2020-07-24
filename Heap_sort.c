ec277dea68a5b3e37ed84a7b8b8be847a0afe709
CC_peng <394995419@qq.com>
2020-07-24 11:16:32 +0800

    add heap sort

---
+++ Heap_sort.c
@@ -0,0 +1,65 @@
+//heap sort
+#include <stdio.h>
+#include <memory.h>
+#include <stdlib.h>
+#include "public.h"
+
+#define PARENT(i) i/2
+#define LEFT(i) (i)*2
+#define RIGHT(i) (i)*2 + 1
+
+void Max_Heapify(int Arr[], int lenth, int index){
+    int left = LEFT(index + 1) - 1;
+    int right = RIGHT(index + 1) - 1;
+    int Max_index = index;
+
+    printf("begin Max_heapify ...");
+    ShowArr(Arr, lenth);
+
+    if(left < lenth && right < lenth){
+        Max_index = Arr[left] > Arr[right] ? left : right;
+        Max_index = Arr[index] > Arr[Max_index] ? index : Max_index;
+    }else if(left < lenth){
+        Max_index = Arr[left] > Arr[index] ? left : index;
+    }else if(right < lenth){
+        Max_index = Arr[right] > Arr[index] ? right : index;
+    }else{
+        Max_index = index;
+    }
+    printf("Max_index ..%d\n", Max_index);
+    if(Max_index != index){
+        int tmp = Arr[index];
+        Arr[index] = Arr[Max_index];
+        Arr[Max_index] = tmp;
+        Max_Heapify(Arr, lenth, Max_index);
+    }
+}
+
+void Build_Max_Heap(int Arr[], int lenth){
+    int index = lenth/2;
+    int i = 0;
+    for(i = index - 1; i >= 0 ; i--){
+        Max_Heapify(Arr, lenth, i);
+    }
+}
+
+void Heap_sort(int Arr[], int lenth){
+    Build_Max_Heap(Arr,lenth);
+
+    int i = 0;
+
+    for(i = lenth -1; i > 0; i--){
+        int tmp = Arr[i];
+        Arr[i] = Arr[0];
+        Arr[0] = tmp;
+        Max_Heapify(Arr, i, 0);
+    }
+}
+
+void main(){
+    int Arr[] = {23, 17, 14, 6, 13, 10, 1, 5, 7, 12};
+    ShowArr(Arr, sizeof(Arr)/sizeof(int));
+    printf("Arr_lenth %d\n", sizeof(Arr)/sizeof(int));
+    Heap_sort(Arr, sizeof(Arr)/sizeof(int));
+    ShowArr(Arr, sizeof(Arr)/sizeof(int));
+}
--- Makefile
+++ Makefile
@@ -5,18 +5,23 @@ none :
 	@echo "insertion (插入排序)"
 	@echo "bubble (冒泡排序)"
 	@echo "merge (归并排序)"
+	@echo "heap (堆排序(最大堆))"
 
 insertion : $(insertion_obj)
 	gcc -o insertion.e $(insertion_obj)
-	./insertion
+	./insertion.e
 
 bubble : $(bubble_obj)
 	gcc -o bubble.e $(bubble_obj)
-	./bubble
+	./bubble.e
 
 merge : $(merge_obj)
 	gcc -o merge.e $(merge_obj)
-	./merge
+	./merge.e
+
+heap : $(heap_obj)
+	gcc -o heap.e $(heap_obj)
+	./heap.e
 
 .PHONY : clean
 clean :
